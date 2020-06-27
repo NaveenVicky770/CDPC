184472b717947b7c75728b9c0b8681da1da643f4
VickyNKN <o161625@rguktong.ac.in>
2020-06-27 11:03:24 +0530

    Header and footer optimized

--- home.php
+++ home.php
@@ -1,26 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    <!--BootStrap assets hosted from CDN(Content Delivery Network for faster loading of pages) -->
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-    <!-- Popper JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-  </head>
-  
+<?php include('includes/header.html'); ?>
+<?php include('includes/nav_bar.html'); ?>
   <body>
-       <?php include('includes/header.html'); ?>
-       <?php include('includes/nav_bar.html'); ?>
+       
        <br>
        <br>
        <div class="col-md-12">
@@ -63,12 +44,7 @@
          </div>
       </div>
    </div>
-       
-       
-       
-       
-       <?php include('includes/footer.html'); ?>
+    
+      <?php include('includes/footer.html'); ?>
       
-  </body>
   
-</html>
