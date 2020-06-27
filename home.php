2295e5d47020eeb601d810cc80de869f1d1da3d5
VickyNKN <o161625@rguktong.ac.in>
2020-06-27 09:48:48 +0530

    Home page body with cards of chancellor and director introducing

--- home.php
+++ home.php
@@ -21,6 +21,52 @@
   <body>
        <?php include('includes/header.html'); ?>
        <?php include('includes/nav_bar.html'); ?>
+       <br>
+       <br>
+       <div class="col-md-12">
+      <div class="row">
+         <div class="col-md-5">
+            <div class="card">
+               <ul class="list-group list-group-flush">
+                  <li class="list-group-item">
+                     <div class="row no-gutters">
+                        <div class="col-md-4">
+                           <img src="resources/images/chancellor.jpg" class="card-img" alt="Chancellor Image">
+                        </div>
+                        <div class="col-md-8">
+                           <div class="card-body">
+                              <h5 class="card-title">Prof. K.C.Reddy</h5>
+                              <small class="text-muted">Chancellor &amp; Chairman of the Governing Council, RGUKT - A.P.</small>
+                              <p class="card-text">RGUKT, established in 2008, provides qualitative technical education coupled with well equipped infrastructure... </p>
+                           </div>
+                        </div>
+                     </div>
+                  </li>
+                  <li class="list-group-item">
+                     <div class="row no-gutters">
+                        <div class="col-md-4">
+                           <img src="resources/images/director.jpg" class="card-img" alt="Director Image">
+                        </div>
+                        <div class="col-md-8">
+                           <div class="card-body">
+                              <h5 class="card-title">Prof. D. Suryachandra Rao</h5>
+                              <small class="text-muted">Director, RGUKT Nuzvid</small>
+                              <p class="card-text">Greetings from Rajiv Gandhi University of Knowledge Technologies (RGUKT), Nuzvid, AP.
+                                 It is a great pleasure for...
+                              </p>
+                           </div>
+                        </div>
+                     </div>
+                  </li>
+               </ul>
+            </div>
+         </div>
+      </div>
+   </div>
+       
+       
+       
+       
        <?php include('includes/footer.html'); ?>
       
   </body>
