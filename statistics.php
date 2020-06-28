7668e05d5d2d9b92f575d599cba83ea94308e288
Madh0906 <67431480+Madh0906@users.noreply.github.com>
2020-06-28 10:40:52 +0530

    Statistics page added...

---
+++ statistics.php
@@ -0,0 +1,154 @@
+<?php include( 'includes/header.html' );
+?>
+<?php include( 'includes/nav_bar.html' );
+?>
+
+<body>
+
+    <div class="container">
+        <div class="col-md-14" style="margin-top:50px;">
+            <div class="card">
+                <div class="card-header">Academic Year (2019-2020) Statistics</div>
+                <div class="card-body">
+                    <div class="card">
+                        <div class="card-header">(2020) Batch Statistics</div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-7">
+                                    <div class="table-responsive">
+                                        <table class="table table-stripped table-bordered">
+                                            <thead>
+                                                <th>#</th>
+                                                <th>CSE</th>
+                                                <th>ECE</th>
+                                                <th>MECH</th>
+                                                <th>MME</th>
+                                                <th>CE</th>
+                                                <th>CHEM</th>
+                                                <th>Total</th>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <th>Total</th>
+                                                    <td>268</td>
+                                                    <td>257</td>
+                                                    <td>178</td>
+                                                    <td>56</td>
+                                                    <td>121</td>
+                                                    <td>61</td>
+                                                    <td>941</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Interns</th>
+                                                    <td>85</td>
+                                                    <td>37</td>
+                                                    <td>22</td>
+                                                    <td>4</td>
+                                                    <td>20</td>
+                                                    <td>1</td>
+                                                    <td>169</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>PPO</th>
+                                                    <td>0</td>
+                                                    <td>0</td>
+                                                    <td>0</td>
+                                                    <td>0</td>
+                                                    <td>0</td>
+                                                    <td>0</td>
+                                                    <td>0</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Placed</th>
+                                                    <td>52</td>
+                                                    <td>74</td>
+                                                    <td>18</td>
+                                                    <td>1</td>
+                                                    <td>2</td>
+                                                    <td>6</td>
+                                                    <td>153</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="card">
+                        <div class="card-header">(2021) Batch Statistics</div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-7">
+                                    <div class="table-responsive">
+                                        <table class="table table-stripped table-bordered">
+                                            <thead>
+                                                <th>#</th>
+                                                <th>CSE</th>
+                                                <th>ECE</th>
+                                                <th>MECH</th>
+                                                <th>MME</th>
+                                                <th>CE</th>
+                                                <th>CHEM</th>
+                                                <th>Total</th>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <th>Total</th>
+                                                    <td>268</td>
+                                                    <td>257</td>
+                                                    <td>178</td>
+                                                    <td>56</td>
+                                                    <td>121</td>
+                                                    <td>61</td>
+                                                    <td>941</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Interns</th>
+                                                    <td>85</td>
+                                                    <td>37</td>
+                                                    <td>22</td>
+                                                    <td>4</td>
+                                                    <td>20</td>
+                                                    <td>1</td>
+                                                    <td>169</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>PPO</th>
+                                                    <td>0</td>
+                                                    <td>0</td>
+                                                    <td>0</td>
+                                                    <td>0</td>
+                                                    <td>0</td>
+                                                    <td>0</td>
+                                                    <td>0</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Placed</th>
+                                                    <td>52</td>
+                                                    <td>74</td>
+                                                    <td>18</td>
+                                                    <td>1</td>
+                                                    <td>2</td>
+                                                    <td>6</td>
+                                                    <td>153</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>            
+
+                </div>
+            </div>
+                
+        </div>
+    </div>
+    <br>
+    <br>
+    <br><br>
+    <?php include( 'includes/footer.html' );
+?>
