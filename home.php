c6cc45973011cc57be5301b33331a104aa9d87a6
VickyNKN <o161625@rguktong.ac.in>
2020-06-28 08:57:03 +0530

    Added a bootstrap carousel in home page to display slider

--- home.php
+++ home.php
@@ -1,50 +1,77 @@
-<?php include('includes/header.html'); ?>
-<?php include('includes/nav_bar.html'); ?>
-  <body>
-       
-       <br>
-       <br>
-       <div class="col-md-12">
-      <div class="row">
-         <div class="col-md-5">
-            <div class="card">
-               <ul class="list-group list-group-flush">
-                  <li class="list-group-item">
-                     <div class="row no-gutters">
-                        <div class="col-md-4">
-                           <img src="resources/images/chancellor.jpg" class="card-img" alt="Chancellor Image">
-                        </div>
-                        <div class="col-md-8">
-                           <div class="card-body">
-                              <h5 class="card-title">Prof. K.C.Reddy</h5>
-                              <small class="text-muted">Chancellor &amp; Chairman of the Governing Council, RGUKT - A.P.</small>
-                              <p class="card-text">RGUKT, established in 2008, provides qualitative technical education coupled with well equipped infrastructure... </p>
-                           </div>
-                        </div>
-                     </div>
-                  </li>
-                  <li class="list-group-item">
-                     <div class="row no-gutters">
-                        <div class="col-md-4">
-                           <img src="resources/images/director.jpg" class="card-img" alt="Director Image">
-                        </div>
-                        <div class="col-md-8">
-                           <div class="card-body">
-                              <h5 class="card-title">Prof. D. Suryachandra Rao</h5>
-                              <small class="text-muted">Director, RGUKT Nuzvid</small>
-                              <p class="card-text">Greetings from Rajiv Gandhi University of Knowledge Technologies (RGUKT), Nuzvid, AP.
-                                 It is a great pleasure for...
-                              </p>
-                           </div>
-                        </div>
-                     </div>
-                  </li>
-               </ul>
+<?php include( 'includes/header.html' );
+?>
+<?php include( 'includes/nav_bar.html' );
+?>
+
+<body>
+
+    <br>
+    <br>
+    <div class='container-fluid'>
+        <div class='row'>
+            <div class='col-md-5'>
+                <div class='card'>
+                    <ul class='list-group list-group-flush'>
+                        <li class='list-group-item'>
+                            <div class='row no-gutters'>
+                                <div class='col-md-4'>
+                                    <img src='resources/images/chancellor.jpg' class='card-img' alt='Chancellor Image'>
+                                </div>
+                                <div class='col-md-8'>
+                                    <div class='card-body'>
+                                        <h5 class='card-title'>Prof. K.C.Reddy</h5>
+                                        <small class='text-muted'>Chancellor &amp;
+                                            Chairman of the Governing Council, RGUKT - A.P.</small>
+                                        <p class='card-text'>RGUKT, established in 2008, provides qualitative technical education coupled with well equipped infrastructure... </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class='list-group-item'>
+                            <div class='row no-gutters'>
+                                <div class='col-md-4'>
+                                    <img src='resources/images/director.jpg' class='card-img' alt='Director Image'>
+                                </div>
+                                <div class='col-md-8'>
+                                    <div class='card-body'>
+                                        <h5 class='card-title'>Prof. D. Suryachandra Rao</h5>
+                                        <small class='text-muted'>Director, RGUKT Nuzvid</small>
+                                        <p class='card-text'>Greetings from Rajiv Gandhi University of Knowledge Technologies ( RGUKT ), Nuzvid, AP.
+                                            It is a great pleasure for...
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
-         </div>
-      </div>
-   </div>
-    
-      <?php include('includes/footer.html'); ?>
-      
-  
+            <div class="col-md-6">
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active" style="background:blue; height:420px;">
+                            <img src="..." class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item" style="background:orange; height:420px;">
+                            <img src="..." class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item" style="background:green; height:420px;">
+                            <img src="..." class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <?php include( 'includes/footer.html' );
+?>
