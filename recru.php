72b16119d2e99d38723dc3f6eba58da054164d8e
challanavaneeswarreddy <o161196@rguktong.ac.in>
2020-06-29 12:16:42 +0530

    recruiters page added

---
+++ recru.php
@@ -0,0 +1,173 @@
+<?php include( 'includes/header.html' );?>
+<?php include( 'includes/nav_bar.html' );?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>CDPC RGUKT-Ongole</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+
+<style type="text/css">
+ol li{
+	font-size: 15px;
+	font-weight: normal;
+	font-family: Cursive;
+}
+ol li:before
+{
+	/*content: "» ";*/
+	content: "\261B ";
+	font-size: 20px;
+}
+.list-group-item{
+	border: none;
+}
+</style>
+
+<div class="container-fluid" style="margin-top: 30px;">
+	<div class="row">
+<div class="col-sm-1 col-md-1 col-lg-1 col-xs-1"></div>
+<div class="col-sm-10 col-md-10 col-lg-10 col-xs-10" style="box-shadow: 1px 2px 15px 10px #eee;margin-top: 10px;">
+			<h3 class="text-center" style="font-weight:bold;color: indigo;text-decoration: underline;font-family: sans-serif;">Our Recruiters</h3>
+
+		<div class="row">
+				<div class="col-sm-2"></div>
+			<div class="col-sm-3">
+				<ol class="list-group">	
+					<li class="list-group-item"> Aarvee Associates</li>
+					<li class="list-group-item"> Achala</li>
+					<li class="list-group-item"> Adeptchips</li>
+					<li class="list-group-item"> ADP</li>
+					<li class="list-group-item"> Adaequare</li>
+					<li class="list-group-item"> Alacriti</li>
+					<li class="list-group-item"> Amara Raja</li>
+					<li class="list-group-item"> APSSDC</li>
+					<li class="list-group-item"> Artech</li>
+					<li class="list-group-item"> Apswires</li>
+					<li class="list-group-item"> Atibir Industries</li>
+					<li class="list-group-item"> Bhea</li>
+					<li class="list-group-item"> Capgemini</li>
+					<li class="list-group-item"> Cigitel</li>
+					<li class="list-group-item"> Codon Soft</li>
+					<li class="list-group-item"> Control S</li>
+					<li class="list-group-item"> Coromandel Paints</li>
+					<li class="list-group-item"> Cts</li>
+					<li class="list-group-item"> Curo Health Care</li>
+					<li class="list-group-item"> Cyber Eye</li>
+					<li class="list-group-item"> Darwinbox</li>
+					<li class="list-group-item"> DELL</li>
+					<li class="list-group-item"> Dhruv Soft</li>
+					<li class="list-group-item"> Discovery Labs Pvt Ltd</li>
+					<li class="list-group-item"> Efftronics</li>
+					<li class="list-group-item"> EPAM</li>
+					<li class="list-group-item"> Ever Glades</li>
+					<li class="list-group-item"> Fony</li>
+					<li class="list-group-item"> Fresh Desk</li>
+					<li class="list-group-item"> FSS</li>
+					<li class="list-group-item"> Golden Hills Capitals</li>
+					<li class="list-group-item"> Hetero Drugs</li>
+					<li class="list-group-item"> Hansa Solutions</li>
+					<li class="list-group-item"> Highradius</li>
+					<li class="list-group-item"> Huawei</li>
+					<li class="list-group-item"> IB Group</li>
+					<li class="list-group-item"> IBM</li>
+					<li class="list-group-item"> Impulse</li>
+					<li class="list-group-item"> Incygys</li>
+					<li class="list-group-item"> Infologitech</li>
+				</ol>
+			</div>
+			<div class="col-sm-3">
+				<ol class="list-group">
+					<li class="list-group-item"> Infosys</li>
+					<li class="list-group-item"> Inventiz</li>
+					<li class="list-group-item"> Iotap</li>
+					<li class="list-group-item"> ITC Ltd</li>
+					<li class="list-group-item"> Iviz</li>
+					<li class="list-group-item"> Jochebed</li>
+					<li class="list-group-item"> Jotter-AI</li>
+					<li class="list-group-item"> Keolis</li>
+					<li class="list-group-item"> Kewill</li>
+					<li class="list-group-item"> KITS</li>
+					<li class="list-group-item"> KJ Systems</li>
+					<li class="list-group-item"> KMV</li>
+					<li class="list-group-item"> Kreayooto</li>
+					<li class="list-group-item"> L&T Infotech</li>
+					<li class="list-group-item"> Mahindra & Mahindra</li>
+					<li class="list-group-item"> Chilukuri projects </li>
+					<li class="list-group-item"> Mathworks</li>
+					<li class="list-group-item"> Master PCB</li>
+					<li class="list-group-item"> Medha Servo Drives</li>
+					<li class="list-group-item"> MEIL</li>
+					<li class="list-group-item"> Mount Blue</li>
+					<li class="list-group-item"> Mphasis</li>
+					<li class="list-group-item"> MRF</li>
+					<li class="list-group-item"> Net Enrich</li>
+					<li class="list-group-item"> Nile Stream</li>
+					<li class="list-group-item"> Novus Green</li>
+					<li class="list-group-item"> Nucon Aerospace</li>
+					<li class="list-group-item"> Opentext</li>
+					<li class="list-group-item"> Opsramp</li>
+					<li class="list-group-item"> Osmosys</li>
+					<li class="list-group-item"> O2labs</li>
+					<li class="list-group-item"> Pecu Service Limited</li>
+					<li class="list-group-item"> Phoenix Research & Const Sol</li>
+					<li class="list-group-item"> Pivox Labs</li>
+					<li class="list-group-item"> Prodix</li>
+					<li class="list-group-item"> Purple</li>
+					<li class="list-group-item"> PurpleTalk</li>
+					<li class="list-group-item"> Ramersoft Pvt.Ltd</li>
+				</ol>
+			</div>
+			<div class="col-sm-3">
+				<ol class="list-group">
+					<li class="list-group-item"> Ramky Infra</li>
+					<li class="list-group-item"> Rara Soft Ltd</li>
+					<li class="list-group-item"> Robert Bosch</li>
+					<li class="list-group-item"> Rockwell</li>
+					<li class="list-group-item"> Saksham Strategy group</li>
+					<li class="list-group-item"> Sales Force</li>
+					<li class="list-group-item"> Scriptbee</li>
+					<li class="list-group-item"> Selekt</li>
+					<li class="list-group-item"> Sevya Multimedia</li>
+					<li class="list-group-item"> Sheela Group</li>
+					<li class="list-group-item"> Smaat India</li>
+					<li class="list-group-item"> Src Tech</li>
+					<li class="list-group-item"> Synopsys</li>
+					<li class="list-group-item"> Tara Mobile</li>
+					<li class="list-group-item"> TCS</li>
+					<li class="list-group-item"> Tech Mahindra</li>
+					<li class="list-group-item"> Techvedhika</li>
+					<li class="list-group-item"> Tessolve Semiconductors</li>
+					<li class="list-group-item"> Texas Instruments</li>
+					<li class="list-group-item"> Thoughtworks</li>
+					<li class="list-group-item"> Triniti</li>
+					<li class="list-group-item"> Uurmi</li>
+					<li class="list-group-item"> Vassar Labs</li>
+					<li class="list-group-item"> Vem Technologies</li>
+					<li class="list-group-item"> Vistara IT</li>			
+					<li class="list-group-item"> Vindhya Industries</li>
+					<li class="list-group-item"> Vitara</li>
+					<li class="list-group-item"> VWR Labs</li>
+					<li class="list-group-item"> Way2Online</li>
+					<li class="list-group-item"> Wipro</li>
+					<li class="list-group-item"> WisdomKites</li>
+					<li class="list-group-item"> Webliapps</li>
+					<li class="list-group-item"> WudPlay</li>
+					<li class="list-group-item"> Zen3</li>
+					<li class="list-group-item"> ZEBI Data India LTD</li>
+					<li class="list-group-item"> 100 pins</li>
+				</ol>
+			</div>
+		</div>
+		</div>
+</body>
+</html>
+<?php include( 'includes/footer.html' );
+?>
