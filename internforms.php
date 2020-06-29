53a080419c6b6d1610b9cb2ae809903549bdfe4c
VickyNKN <o161625@rguktong.ac.in>
2020-06-29 14:46:47 +0530

    Forms page added

---
+++ internforms.php
@@ -0,0 +1,159 @@
+<?php include( 'includes/header.html' );?>
+<?php include( 'includes/nav_bar.html' );?>
+
+<body>
+    <div class="container-fluid" style="margin-top:50px;">
+        <div class="col-md-12">
+            <div class="row">
+               
+                <div class="col-3">
+                    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                        <a class="nav-link nav-me" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
+                        <a class="nav-link nav-me" id="v-pills-summer-tab" data-toggle="pill" href="#v-pills-summer" role="tab" aria-controls="v-pills-summer" aria-selected="false">Summer Internship Forms</a>
+                        <a class="nav-link nav-me" id="v-pills-longterm-tab" data-toggle="pill" href="#v-pills-longterm" role="tab" aria-controls="v-pills-longterm" aria-selected="false">Long Term Internship Forms</a>
+                    </div>
+                </div>
+                
+                <div class="col-9">
+                    <div class="tab-content" id="v-pills-tabContent">
+                       
+                        <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+                            <li><i class="fa fa-hand-o-right"></i> Step-1 : Download Forms from this section as per your requirement.Take a xerox.Fill Carefully</li>
+                            <li><i class="fa fa-hand-o-right"></i> Step-2 : Now go for signatures (Your Project Guide , DPN , HOD) then contact CDPC Cell for verification</li>
+                            <!-- <li><i class="fa fa-hand-o-right"></i> Step-3 : If You are male then go to Dean Academics Office directly. If not go for DSW Office then go for Dean Academics Offices </li> -->
+                        </div>
+                        
+                        <!-- Placements Body Starts-->
+                        <div class="tab-pane fade" id="v-pills-summer" role="tabpanel" aria-labelledby="v-pills-summer-tab">
+                            <div class="alert alert-info">
+                                <i class="fa fa-hand-o-right"></i> <b>Forms to be submitted before leaving the campus to Summer Internship</b>
+                            </div>
+                            
+                            <!--Table froms to be submitted before leaving campus-->
+                            
+                            <table class="table table-bordered table-stripped">
+                                <thead>
+                                    <tr>
+                                        <th>Filename</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td><i class="fa fa-arrow-circle-right"></i> Student Application format (Summer Internship Annexure-1)</td>
+                                        <td><button class="btn btn-sm btn-success" href="forms/Summer/Student Application format (Internship Annexure-1).pdf"><i class="fa fa-download"></i></button></td>
+                                    </tr>
+                                    <tr>
+                                        <td><i class="fa fa-arrow-circle-right"></i> Undertaking form (Internship Annexure 2) </td>
+                                        <td><button class="btn btn-sm btn-success" href="forms/Summer/Undertaking form (Internship Annexure 2).pdf"><i class="fa fa-download"></i></button></td>
+                                    </tr>
+                                    <tr>
+                                        <td><i class="fa fa-arrow-circle-right"></i> No Objection Certificate (Summer Internship Annexure 3)</td>
+                                        <td><button class="btn btn-sm btn-success" href="forms/Summer/No Objection Certificate (Internship Annexure 3).pdf"><i class="fa fa-download"></i></button></td>
+                                    </tr>
+                                    <tr>
+                                        <td><i class="fa fa-arrow-circle-right"></i> Student-Leave-application-form-for-Biometric-attendance</td>
+                                        <td><button class="btn btn-sm btn-success" href="forms/Student-Leave-application-form-for-Biometric-attendance.pdf"><i class="fa fa-download"></i></button></td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                            
+                            <div class="alert alert-info">
+                                <i class="fa fa-hand-o-right"></i> <b>Forms to be submitted before coming to the campus from Summer
+                                    Internship for completing examination work</b>
+                            </div>
+                            
+                            
+                            <!--Table froms to be submitted before Coming to campus-->
+                            <table class="table table-bordered table-stripped">
+                                <thead>
+                                    <tr>
+                                        <th>Filename</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td><i class="fa fa-arrow-circle-right"></i> Relieving certificate (Summer Internship Annexure-4)</td>
+                                        <td><button class="btn btn-sm btn-success" href="forms/Summer/Relieving certificate (Internship Annexure-4).pdf"><i class="fa fa-download"></i></button></td>
+                                    </tr>
+                                    <tr>
+                                        <td><i class="fa fa-arrow-circle-right"></i> Evaluation form (Summer Internship Annexure-5)</td>
+                                        <td><button class="btn btn-sm btn-success" href="forms/Summer/Evaluation form (Internship Annexure-5).pdf"><i class="fa fa-download"></i></button></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <br>
+                            <br>
+                            <br>
+                        </div>
+                        <!-- Summer Body Starts-->
+                        <div class="tab-pane fade" id="v-pills-longterm" role="tabpanel" aria-labelledby="v-pills-longterm-tab">
+                            <div class="alert alert-info">
+                                <i class="fa fa-hand-o-right"></i> <b>Forms to be submitted before leaving the campus to Long Term Internship</b>
+                            </div>
+                            <table class="table table-bordered table-stripped">
+                                <thead>
+                                    <tr>
+                                        <th>Filename</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td><i class="fa fa-arrow-circle-right"></i> Student Application format (Longterm Internship Annexure-1)</td>
+                                        <td><button class="btn btn-sm btn-success" href="forms/Longterm/Student Application format (Internship Annexure-1).pdf"><i class="fa fa-download"></i></button></td>
+                                    </tr>
+                                    <tr>
+                                        <td><i class="fa fa-arrow-circle-right"></i> Undertaking form (Longterm Internship Annexure 2)</td>
+                                        <td><button class="btn btn-sm btn-success" href="forms/Longterm/Undertaking form (Internship Annexure 2).pdf"><i class="fa fa-download"></i></button></td>
+                                    </tr>
+                                    <tr>
+                                        <td><i class="fa fa-arrow-circle-right"></i> No Objection Certificate (Longterm Internship Annexure 3)</td>
+                                        <td><button class="btn btn-sm btn-success" href="forms/Longterm/No Objection Certificate (Internship Annexure 3).pdf"><i class="fa fa-download"></i></button></td>
+                                    </tr>
+                                    <tr>
+                                        <td><i class="fa fa-arrow-circle-right"></i> Student-Leave-application-form-for-Biometric-attendance</td>
+                                        <td><button class="btn btn-sm btn-success" href="forms/Student-Leave-application-form-for-Biometric-attendance.pdf"><i class="fa fa-download"></i></button></td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                            <div class="alert alert-info">
+                                <i class="fa fa-hand-o-right"></i> <b> Forms to be submitted before coming to the campus from Long Term Internship for completing examination work</b>
+                            </div>
+                            <table class="table table-bordered table-stripped">
+                                <thead>
+                                    <tr>
+                                        <th>Filename</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td><i class="fa fa-arrow-circle-right"></i> Relieving certificate (Longterm Internship Annexure-4)</td>
+                                        <td><button class="btn btn-sm btn-success" href="forms/Longterm/Relieving certificate (Internship Annexure-4).pdf"><i class="fa fa-download"></i></button></td>
+                                    </tr>
+                                    <tr>
+                                        <td><i class="fa fa-arrow-circle-right"></i> Evaluation form (Longterm Internship Annexure-5)</td>
+                                        <td><button class="btn btn-sm btn-success" href="forms/Longterm/Evaluation form (Internship Annexure-5).pdf"><i class="fa fa-download"></i></button></td>
+                                    </tr>
+                                    <tr>
+                                        <td><i class="fa fa-arrow-circle-right"></i>Relieving Letter (Longterm Internship Annexure-6)</td>
+                                        <td><button class="btn btn-sm btn-success" href="forms/Longterm/Relieving Letter (Internship Annexure-6).pdf"><i class="fa fa-download"></i></button></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <br>
+                            <br>
+                            <br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+<?php include( 'includes/footer.html' );?>
