0175e30efd1ea5e3f5119bca90cb6fdd6a4902a9
Madh0906 <67431480+Madh0906@users.noreply.github.com>
2020-06-29 18:26:41 +0530

    Contact us page added .....

---
+++ contactus.php
@@ -0,0 +1,288 @@
+<?php include( 'includes/header.html' );
+?>
+<?php include( 'includes/nav_bar.html' );
+?>
+<head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <style type="text/css">
+      .material-icons{
+          display: inline-flex;
+          vertical-align: top;
+          font-size: 22px;
+        }    
+  </style>
+</head>
+<body>
+<div class="container mt-4">
+<span style="font-size: 24px; font-weight: bold;">Office</span><br>
+<span style="font-size: 16px;">You can contact us through mail</span><br>
+<div class="row">
+    <div class="col-6">
+      <div class="card mt-2">
+        <div class="row no-gutters">
+          <div class="col-md-2">
+            <img src="resources/CDPC.jpg" class="card-img w-90 ml-2 mt-2 h-65 p-2 rounded" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body p-2 ml-3">
+                <span style="font-weight: bold; font-size: 20px;">Mr. P. Shyam</span><br>
+                <span style="font-size: 14px;">Campus Placement Cell Incharge(I/c)</span><br>
+                <span style="font-size: 14px;"><i class="material-icons">phone</i> 0866-248532</span><br>
+                <span style="font-size: 14px;"><i class="material-icons">mail</i> tnpni@rgukt.in</span><br>
+            </div>
+          </div>
+        </div> 
+      </div>
+    </div>
+    <div class="col-6">
+        <div class="card mt-2">
+          <div class="row no-gutters">
+            <div class="col-md-2">
+              <img src="resources/CDPC.jpg" class="card-img w-90 ml-2 mt-2 h-65 p-2 rounded" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body p-2 ml-3">
+                  <span style="font-weight: bold; font-size: 20px;">Mr. P. Shyam</span><br>
+                  <span style="font-size: 14px;">Campus Placement Cell Incharge(I/c)</span><br>
+                  <span style="font-size: 14px;"><i class="material-icons">phone</i> 0866-248532</span><br>
+                  <span style="font-size: 14px;"><i class="material-icons">mail</i> tnpni@rgukt.in</span><br>
+                </div>
+            </div>
+          </div> 
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="container mt-4">
+<div class="row">
+    <div class="col-6">
+      <div class="card ">
+        <div class="row no-gutters">
+          <div class="col-md-2">
+            <img src="resources/CDPC.jpg" class="card-img w-90 ml-2 mt-2 h-65 p-2 rounded" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body p-2 ml-3">
+                <span style="font-weight: bold; font-size: 20px;">Mr. P. Shyam</span><br>
+                <span style="font-size: 14px;">Campus Placement Cell Incharge(I/c)</span><br>
+                <span style="font-size: 14px;"><i class="material-icons">phone</i> 0866-248532</span><br>
+                <span style="font-size: 14px;"><i class="material-icons">mail</i> tnpni@rgukt.in</span><br>
+            </div>
+          </div>
+        </div> 
+      </div>
+    </div>
+    <div class="col-6">
+        <div class="card ">
+          <div class="row no-gutters">
+            <div class="col-md-2">
+              <img src="resources/CDPC.jpg" class="card-img w-90 ml-2 mt-2 h-65 p-2 rounded" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body p-2 ml-3">
+                  <span style="font-weight: bold; font-size: 20px;">Mr. P. Shyam</span><br>
+                  <span style="font-size: 14px;">Campus Placement Cell Incharge(I/c)</span><br>
+                  <span style="font-size: 14px;"><i class="material-icons">phone</i> 0866-248532</span><br>
+                  <span style="font-size: 14px;"><i class="material-icons">mail</i> tnpni@rgukt.in</span><br>
+                </div>
+            </div>
+          </div> 
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="container mt-4">
+<div class="row">
+    <div class="col-6">
+      <div class="card ">
+        <div class="row no-gutters">
+          <div class="col-md-2">
+            <img src="resources/CDPC.jpg" class="card-img w-90 ml-2 mt-2 h-65 p-2 rounded" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body p-2 ml-3">
+                <span style="font-weight: bold; font-size: 20px;">Mr. P. Shyam</span><br>
+                <span style="font-size: 14px;">Campus Placement Cell Incharge(I/c)</span><br>
+                <span style="font-size: 14px;"><i class="material-icons">phone</i> 0866-248532</span><br>
+                <span style="font-size: 14px;"><i class="material-icons">mail</i> tnpni@rgukt.in</span><br>
+            </div>
+          </div>
+        </div> 
+      </div>
+    </div>
+    <div class="col-6">
+        <div class="card ">
+          <div class="row no-gutters">
+            <div class="col-md-2">
+              <img src="resources/CDPC.jpg" class="card-img w-90 ml-2 mt-2 h-65 p-2 rounded" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body p-2 ml-3">
+                  <span style="font-weight: bold; font-size: 20px;">Mr. P. Shyam</span><br>
+                  <span style="font-size: 14px;">Campus Placement Cell Incharge(I/c)</span><br>
+                  <span style="font-size: 14px;"><i class="material-icons">phone</i> 0866-248532</span><br>
+                  <span style="font-size: 14px;"><i class="material-icons">mail</i> tnpni@rgukt.in</span><br>
+                </div>
+            </div>
+          </div> 
+        </div>
+    </div>
+</div>
+</div>
+
+
+
+<div class="container mt-4">
+<span style="font-size: 24px; font-weight: bold;">DPN's</span><br>
+<span style="font-size: 16px;">You can contact us through mail</span><br>
+<div class="row">
+    <div class="col-6">
+      <div class="card mt-2">
+        <div class="row no-gutters">
+          <div class="col-md-2">
+            <img src="resources/CDPC.jpg" class="card-img w-90 ml-2 mt-2 h-65 p-2 rounded" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body p-2 ml-3">
+                <span style="font-weight: bold; font-size: 20px;">Mr. P. Shyam</span><br>
+                <span style="font-size: 14px;">Campus Placement Cell Incharge(I/c)</span><br>
+                <span style="font-size: 14px;"><i class="material-icons">phone</i> 0866-248532</span><br>
+                <span style="font-size: 14px;"><i class="material-icons">mail</i> tnpni@rgukt.in</span><br>
+            </div>
+          </div>
+        </div> 
+      </div>
+    </div>
+    <div class="col-6">
+        <div class="card mt-2">
+          <div class="row no-gutters">
+            <div class="col-md-2">
+              <img src="resources/CDPC.jpg" class="card-img w-90 ml-2 mt-2 h-65 p-2 rounded" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body p-2 ml-3">
+                  <span style="font-weight: bold; font-size: 20px;">Mr. P. Shyam</span><br>
+                  <span style="font-size: 14px;">Campus Placement Cell Incharge(I/c)</span><br>
+                  <span style="font-size: 14px;"><i class="material-icons">phone</i> 0866-248532</span><br>
+                  <span style="font-size: 14px;"><i class="material-icons">mail</i> tnpni@rgukt.in</span><br>
+                </div>
+            </div>
+          </div> 
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="container mt-4">
+<div class="row">
+    <div class="col-6">
+      <div class="card ">
+        <div class="row no-gutters">
+          <div class="col-md-2">
+            <img src="resources/CDPC.jpg" class="card-img w-90 ml-2 mt-2 h-65 p-2 rounded" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body p-2 ml-3">
+                <span style="font-weight: bold; font-size: 20px;">Mr. P. Shyam</span><br>
+                <span style="font-size: 14px;">Campus Placement Cell Incharge(I/c)</span><br>
+                <span style="font-size: 14px;"><i class="material-icons">phone</i> 0866-248532</span><br>
+                <span style="font-size: 14px;"><i class="material-icons">mail</i> tnpni@rgukt.in</span><br>
+            </div>
+          </div>
+        </div> 
+      </div>
+    </div>
+    <div class="col-6">
+        <div class="card ">
+          <div class="row no-gutters">
+            <div class="col-md-2">
+              <img src="resources/CDPC.jpg" class="card-img w-90 ml-2 mt-2 h-65 p-2 rounded" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body p-2 ml-3">
+                  <span style="font-weight: bold; font-size: 20px;">Mr. P. Shyam</span><br>
+                  <span style="font-size: 14px;">Campus Placement Cell Incharge(I/c)</span><br>
+                  <span style="font-size: 14px;"><i class="material-icons">phone</i> 0866-248532</span><br>
+                  <span style="font-size: 14px;"><i class="material-icons">mail</i> tnpni@rgukt.in</span><br>
+                </div>
+            </div>
+          </div> 
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="container mt-4">
+<div class="row">
+    <div class="col-6">
+      <div class="card ">
+        <div class="row no-gutters">
+          <div class="col-md-2">
+            <img src="resources/CDPC.jpg" class="card-img w-90 ml-2 mt-2 h-65 p-2 rounded" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body p-2 ml-3">
+                <span style="font-weight: bold; font-size: 20px;">Mr. P. Shyam</span><br>
+                <span style="font-size: 14px;">Campus Placement Cell Incharge(I/c)</span><br>
+                <span style="font-size: 14px;"><i class="material-icons">phone</i> 0866-248532</span><br>
+                <span style="font-size: 14px;"><i class="material-icons">mail</i> tnpni@rgukt.in</span><br>
+            </div>
+          </div>
+        </div> 
+      </div>
+    </div>
+    <div class="col-6">
+        <div class="card ">
+          <div class="row no-gutters">
+            <div class="col-md-2">
+              <img src="resources/CDPC.jpg" class="card-img w-90 ml-2 mt-2 h-65 p-2 rounded" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body p-2 ml-3">
+                  <span style="font-weight: bold; font-size: 20px;">Mr. P. Shyam</span><br>
+                  <span style="font-size: 14px;">Campus Placement Cell Incharge(I/c)</span><br>
+                  <span style="font-size: 14px;"><i class="material-icons">phone</i> 0866-248532</span><br>
+                  <span style="font-size: 14px;"><i class="material-icons">mail</i> tnpni@rgukt.in</span><br>
+                </div>
+            </div>
+          </div> 
+        </div>
+    </div>
+</div>
+</div>
+
+
+
+<div class="container mt-4 mb-4">
+<span style="font-size: 24px; font-weight: bold;">Web Team</span><br>
+<span style="font-size: 16px;">You can contact us through mail</span><br>
+<div class="row">
+    <div class="col-6">
+      <div class="card mt-2">
+        <div class="row no-gutters">
+          <div class="col-md-2">
+            <img src="resources/CDPC.jpg" class="card-img w-90 ml-2 mt-2 h-65 p-2 rounded" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body p-2 ml-3">
+                <span style="font-weight: bold; font-size: 20px;">CDPC</span><br>
+                <span style="font-size: 14px;">Campus Placement Cell Incharge(I/c)</span><br>
+                <span style="font-size: 14px;"><i class="material-icons">phone</i> 0866-248532</span><br>
+                <span style="font-size: 14px;"><i class="material-icons">mail</i> tnpni@rgukt.in</span><br>
+            </div>
+          </div>
+        </div> 
+      </div>
+    </div>
+</div>
+</div>
+
+
+   
+  <br>
+  <br>
+ 
+ <?php include( 'includes/footer.html' );
+?>
