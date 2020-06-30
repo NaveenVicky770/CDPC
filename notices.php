b8120d86aedb27cbc639ad0d272cd14dc8017d71
VickyNKN <o161625@rguktong.ac.in>
2020-06-30 17:45:27 +0530

    notifications page added

---
+++ notices.php
@@ -0,0 +1,153 @@
+<?php include( 'includes/header.html' );?>
+<?php include( 'includes/nav_bar.html' );?>
+
+<body>
+    <div class="container-fluid" style="margin-top:50px;">
+        <div class="col-md-12">
+            <div id="dataTables-example_wrapper" class="dataTables_wrapper dt-bootstrap4 no-footer">
+                <div class="row">
+                   
+                    <div class="col-sm-12 col-md-6">
+                        <div class="dataTables_length" id="dataTables-example_length"><label>Show <select name="dataTables-example_length" aria-controls="dataTables-example" class="custom-select custom-select-sm form-control form-control-sm">
+                                    <option value="10">10</option>
+                                    <option value="25">25</option>
+                                    <option value="50">50</option>
+                                    <option value="100">100</option>
+                                </select> entries</label></div>
+                    </div>
+                    
+                    <div class="col-sm-12 col-md-6">
+                        <div id="dataTables-example_filter" class="dataTables_filter">
+                        <label>Search:<input type="search" class="form-control form-control-sm" placeholder="search here" aria-controls="dataTables-example"></label></div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <table class="table table-striped table-bordered table-responsive dataTable no-footer" id="dataTables-example" role="grid" aria-describedby="dataTables-example_info">
+                            <thead>
+                                <tr role="row">
+                                    <th class="sorting_disabled" rowspan="1" colspan="1" style="width: 31px;">Sno</th>
+                                    <th class="sorting_disabled" rowspan="1" colspan="1" style="width: 1055px;">Subject</th>
+                                    <th class="sorting_disabled" rowspan="1" colspan="1" style="width: 53.4px;">To</th>
+                                    <th class="sorting_disabled" rowspan="1" colspan="1" style="width: 149.4px;">Sent Time</th>
+                                    <th class="sorting_disabled" rowspan="1" colspan="1" style="width: 44px;">Views</th>
+                                </tr>
+                            </thead>
+                            
+                            <tbody>
+                                <tr role="row" class="odd">
+                                    <td><span class="badge badge-info">488</span></td>
+                                    <td>
+                                        <a href="#" style="font-family:Helvetica Neue, Helvetica, Arial, sans-serif !important;text-transform:capitalize !important;" data-toggle="modal" data-target="#view-modal" data-id="488" id="getUser">Regarding registration of Vibrant Pharma Drive for E4 CHEM</a></td>
+                                    <td><span class="badge badge-success">E4</span></td>
+                                    <td><span class="badge badge-info">2020-06-05 11:29:13 AM</span></td>
+                                    <td><span class="badge badge-danger">19</span></td>
+                                </tr>
+                                <tr role="row" class="even">
+                                    <td><span class="badge badge-info">487</span></td>
+                                    <td>
+                                        <a href="#" style="font-family:Helvetica Neue, Helvetica, Arial, sans-serif !important;text-transform:capitalize !important;" data-toggle="modal" data-target="#view-modal" data-id="487" id="getUser">Regarding registrations for Digitalkites from 29th April 2020 12:00 PM to 30th April 2020 12:00 PM through Web Portal and Mobile App For E4 CSE ECE</a></td>
+                                    <td><span class="badge badge-success">E4</span></td>
+                                    <td><span class="badge badge-info">2020-04-29 11:58:07 AM</span></td>
+                                    <td><span class="badge badge-danger">134</span></td>
+                                </tr>
+                                <tr role="row" class="odd">
+                                    <td><span class="badge badge-info">486</span></td>
+                                    <td>
+                                        <a href="#" style="font-family:Helvetica Neue, Helvetica, Arial, sans-serif !important;text-transform:capitalize !important;" data-toggle="modal" data-target="#view-modal" data-id="486" id="getUser">Regarding Techincal Survey - E3</a></td>
+                                    <td><span class="badge badge-success">E3</span></td>
+                                    <td><span class="badge badge-info">2020-04-14 06:20:01 PM</span></td>
+                                    <td><span class="badge badge-danger">123</span></td>
+                                </tr>
+                                <tr role="row" class="even">
+                                    <td><span class="badge badge-info">485</span></td>
+                                    <td>
+                                        <a href="#" style="font-family:Helvetica Neue, Helvetica, Arial, sans-serif !important;text-transform:capitalize !important;" data-toggle="modal" data-target="#view-modal" data-id="485" id="getUser">Regarding Daily Tasks Survey E3</a></td>
+                                    <td><span class="badge badge-success">E3</span></td>
+                                    <td><span class="badge badge-info">2020-04-14 06:18:49 PM</span></td>
+                                    <td><span class="badge badge-danger">132</span></td>
+                                </tr>
+                                <tr role="row" class="odd">
+                                    <td><span class="badge badge-info">484</span></td>
+                                    <td>
+                                        <a href="#" style="font-family:Helvetica Neue, Helvetica, Arial, sans-serif !important;text-transform:capitalize !important;" data-toggle="modal" data-target="#view-modal" data-id="484" id="getUser">Regarding E3 students to find the shortlisted students details from Alacriti </a></td>
+                                    <td><span class="badge badge-success">E3</span></td>
+                                    <td><span class="badge badge-info">2020-03-16 09:45:59 AM</span></td>
+                                    <td><span class="badge badge-danger">406</span></td>
+                                </tr>
+                                <tr role="row" class="even">
+                                    <td><span class="badge badge-info">483</span></td>
+                                    <td>
+                                        <a href="#" style="font-family:Helvetica Neue, Helvetica, Arial, sans-serif !important;text-transform:capitalize !important;" data-toggle="modal" data-target="#view-modal" data-id="483" id="getUser">Regarding People Tech On Campus Drive for E4 CSE &amp; ECE Registered on 14th March 2020</a></td>
+                                    <td><span class="badge badge-success">E4</span></td>
+                                    <td><span class="badge badge-info">2020-03-13 04:19:49 PM</span></td>
+                                    <td><span class="badge badge-danger">173</span></td>
+                                </tr>
+                                <tr role="row" class="odd">
+                                    <td><span class="badge badge-info">482</span></td>
+                                    <td>
+                                        <a href="#" style="font-family:Helvetica Neue, Helvetica, Arial, sans-serif !important;text-transform:capitalize !important;" data-toggle="modal" data-target="#view-modal" data-id="482" id="getUser">Regarding People Tech On Campus Drive For E4 CSE &amp; ECE Registered on 14th Mar 2020</a></td>
+                                    <td><span class="badge badge-success">E4</span></td>
+                                    <td><span class="badge badge-info">2020-03-13 11:35:12 AM</span></td>
+                                    <td><span class="badge badge-danger">183</span></td>
+                                </tr>
+                                <tr role="row" class="even">
+                                    <td><span class="badge badge-info">481</span></td>
+                                    <td>
+                                        <a href="#" style="font-family:Helvetica Neue, Helvetica, Arial, sans-serif !important;text-transform:capitalize !important;" data-toggle="modal" data-target="#view-modal" data-id="481" id="getUser">Regarding Summer Internship at IIT Madras </a></td>
+                                    <td><span class="badge badge-success">E3</span></td>
+                                    <td><span class="badge badge-info">2020-03-12 05:07:49 PM</span></td>
+                                    <td><span class="badge badge-danger">300</span></td>
+                                </tr>
+                                <tr role="row" class="odd">
+                                    <td><span class="badge badge-info">480</span></td>
+                                    <td>
+                                        <a href="#" style="font-family:Helvetica Neue, Helvetica, Arial, sans-serif !important;text-transform:capitalize !important;" data-toggle="modal" data-target="#view-modal" data-id="480" id="getUser">Regarding SummerIntern at IIIT Hyderabad</a></td>
+                                    <td><span class="badge badge-success">E3</span></td>
+                                    <td><span class="badge badge-info">2020-03-12 04:29:58 PM</span></td>
+                                    <td><span class="badge badge-danger">386</span></td>
+                                </tr>
+                                <tr role="row" class="even">
+                                    <td><span class="badge badge-info">479</span></td>
+                                    <td>
+                                        <a href="#" style="font-family:Helvetica Neue, Helvetica, Arial, sans-serif !important;text-transform:capitalize !important;" data-toggle="modal" data-target="#view-modal" data-id="479" id="getUser">Alacriti Written Shortlisted for Technical Interviews at I3 CDPC on 12th Mar 2020 from 2:15 PM </a></td>
+                                    <td><span class="badge badge-success">E3</span></td>
+                                    <td><span class="badge badge-info">2020-03-12 10:49:07 AM</span></td>
+                                    <td><span class="badge badge-danger">335</span></td>
+                                </tr>
+                            </tbody>
+                            
+                        </table>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class="col-sm-12 col-md-5">
+                        <div class="dataTables_info" id="dataTables-example_info" role="status" aria-live="polite">Showing 1 to 10 of 462 entries</div>
+                    </div>
+                    <div class="col-sm-12 col-md-7">
+                        <div class="dataTables_paginate paging_simple_numbers" id="dataTables-example_paginate">
+                            <ul class="pagination">
+                                <li class="paginate_button page-item previous disabled" id="dataTables-example_previous"><a href="#" aria-controls="dataTables-example" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li>
+                                <li class="paginate_button page-item active"><a href="#" aria-controls="dataTables-example" data-dt-idx="1" tabindex="0" class="page-link">1</a></li>
+                                <li class="paginate_button page-item "><a href="#" aria-controls="dataTables-example" data-dt-idx="2" tabindex="0" class="page-link">2</a></li>
+                                <li class="paginate_button page-item "><a href="#" aria-controls="dataTables-example" data-dt-idx="3" tabindex="0" class="page-link">3</a></li>
+                                <li class="paginate_button page-item "><a href="#" aria-controls="dataTables-example" data-dt-idx="4" tabindex="0" class="page-link">4</a></li>
+                                <li class="paginate_button page-item "><a href="#" aria-controls="dataTables-example" data-dt-idx="5" tabindex="0" class="page-link">5</a></li>
+                                <li class="paginate_button page-item disabled" id="dataTables-example_ellipsis"><a href="#" aria-controls="dataTables-example" data-dt-idx="6" tabindex="0" class="page-link">…</a></li>
+                                <li class="paginate_button page-item "><a href="#" aria-controls="dataTables-example" data-dt-idx="7" tabindex="0" class="page-link">47</a></li>
+                                <li class="paginate_button page-item next" id="dataTables-example_next"><a href="#" aria-controls="dataTables-example" data-dt-idx="8" tabindex="0" class="page-link">Next</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+</body>
+
+<?php include( 'includes/footer.html' );?>
