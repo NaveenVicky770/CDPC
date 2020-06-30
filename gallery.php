71ffbd59e9924a9200237a80d0f9daab73bb1568
VickyNKN <o161625@rguktong.ac.in>
2020-06-30 19:51:58 +0530

    Gallery page added (basic layout)

---
+++ gallery.php
@@ -0,0 +1,133 @@
+<?php include( 'includes/header.html' );?>
+<?php include( 'includes/nav_bar.html' );?>
+
+<body>
+    <h2 style="text-align:center; margin-top:1rem;">Image Gallery</h2>
+    <div class="row" style="padding:2rem 6rem;">
+        <div class="col-lg-2 col-md-6">
+            <div class="thumbnail">
+                <a href="resources/college.jpg" target="_blank">
+                    <img src="resources/college.jpg" class="img-thumbnail img-responsive" alt="college" style="width:100%">
+                    <div class="caption">
+                        <p>Testing Caption</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-2 col-md-6">
+            <div class="thumbnail">
+                <a href="resources/college.jpg" target="_blank">
+                    <img src="resources/college.jpg" class="img-thumbnail img-responsive" alt="Nature" style="width:100%">
+                    <div class="caption">
+                        <p>Testing Caption</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-2 col-md-6">
+            <div class="thumbnail">
+                <a href="resources/college.jpg" target="_blank">
+                    <img src="resources/college.jpg" class="img-thumbnail img-responsive" alt="Fjords" style="width:100%">
+                    <div class="caption">
+                        <p>Testing Caption</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="col-lg-2 col-md-6">
+            <div class="thumbnail">
+                <a href="resources/college.jpg" target="_blank">
+                    <img src="resources/college.jpg" class="img-thumbnail img-responsive" alt="college" style="width:100%">
+                    <div class="caption">
+                        <p>Testing Caption</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-2 col-md-6">
+            <div class="thumbnail">
+                <a href="resources/college.jpg" target="_blank">
+                    <img src="resources/college.jpg" class="img-thumbnail img-responsive" alt="Nature" style="width:100%">
+                    <div class="caption">
+                        <p>Testing Caption</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-2 col-md-6">
+            <div class="thumbnail">
+                <a href="resources/college.jpg" target="_blank">
+                    <img src="resources/college.jpg" class="img-thumbnail img-responsive" alt="Fjords" style="width:100%">
+                    <div class="caption">
+                        <p>Testing Caption</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-2 col-md-6">
+            <div class="thumbnail">
+                <a href="resources/college.jpg" target="_blank">
+                    <img src="resources/college.jpg" class="img-thumbnail img-responsive" alt="college" style="width:100%">
+                    <div class="caption">
+                        <p>Testing Caption</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-2 col-md-6">
+            <div class="thumbnail">
+                <a href="resources/college.jpg" target="_blank">
+                    <img src="resources/college.jpg" class="img-thumbnail img-responsive" alt="Nature" style="width:100%">
+                    <div class="caption">
+                        <p>Testing Caption</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-2 col-md-6">
+            <div class="thumbnail">
+                <a href="resources/college.jpg" target="_blank">
+                    <img src="resources/college.jpg" class="img-thumbnail img-responsive" alt="Fjords" style="width:100%">
+                    <div class="caption">
+                        <p>Testing Caption</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="col-lg-2 col-md-6">
+            <div class="thumbnail">
+                <a href="resources/college.jpg" target="_blank">
+                    <img src="resources/college.jpg" class="img-thumbnail img-responsive" alt="college" style="width:100%">
+                    <div class="caption">
+                        <p>Testing Caption</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-2 col-md-6">
+            <div class="thumbnail">
+                <a href="resources/college.jpg" target="_blank">
+                    <img src="resources/college.jpg" class="img-thumbnail img-responsive" alt="Nature" style="width:100%">
+                    <div class="caption">
+                        <p>Testing Caption</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-2 col-md-6">
+            <div class="thumbnail">
+                <a href="resources/college.jpg" target="_blank">
+                    <img src="resources/college.jpg" class="img-thumbnail img-responsive" alt="Fjords" style="width:100%">
+                    <div class="caption">
+                        <p>Testing Caption</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+<?php include( 'includes/footer.html' );?>
