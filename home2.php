cd5570516731ad5925276a079e8e0adef6530593
challanavaneeswarreddy <o161196@rguktong.ac.in>
2020-07-01 10:42:32 +0530

    nav bar modified and home page 2(after login) added

---
+++ home2.php
@@ -0,0 +1,82 @@
+<?php include( 'includes/header.html' );?>
+<?php include( 'includes/nav_bar.html' );?>
+
+<body>
+<div id="term_ppo" class="modal fade" role="dialog">
+   <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+         <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalCenterTitle">Rules/Regulations for Termination of Pre Placement Offer</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+            </button>
+         </div>
+         <div class="modal-body">
+            <ul style="font-size: 15px;list-style-type: none;line-height: 40px;">
+               <h4>In the following cases on the mutual agreement the Internship Offer can be terminated: </h4>
+               <li><i class="fa fa-hand-o-right"></i> <u> Medical Grounds:-</u> In this case student should produce the medical certificate from authorized Doctor(<span style="color: red;">Must be a MBBS/Higher</span>)</li>
+               <li><i class="fa fa-hand-o-right"></i> <u>Misconduct of the student:-</u> Company should provide the valid proof against the Misconduct.</li>
+               <li><i class="fa fa-hand-o-right"></i> <u>Performance:-</u> If a student is not found suitable for the full time employment.</li>
+               <li><i class="fa fa-hand-o-right"></i>Not honored with the PPO within the stipulated time meanwhile student got selected in the campus drive.</li>
+               <li><i class="fa fa-hand-o-right"></i>In other cases, student may directly contact the concern Departments.</li>
+            </ul>
+         </div>
+         <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+         </div>
+      </div>
+   </div>
+</div>
+
+<div id="leaves" class="modal fade" role="dialog">
+   <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+         <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalCenterTitle">Rules/Regulations for Taking leaves at Industry</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+            </button>
+         </div>
+         <div class="modal-body">
+            <ul style="font-size: 15px;list-style-type: none;line-height: 40px;">
+               <li><i class="fa fa-hand-o-right"></i> You are requested to either depute (through releiving letter) the students for a period of 20 days during each semester for completion of their examination at our campus or the mutual convenient method for conduction of exams. Without the releiving letter(from company) the student candidature for long-term internship is invalid.</li>
+               <li><i class="fa fa-hand-o-right"></i> Students are available to entitle to avail the leaves as per the company norms.We will not request for any sanction of leave for the student and you are requested to not entertain such requests from students.</li>
+            </ul>
+         </div>
+         <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+         </div>
+      </div>
+   </div>
+</div>
+
+
+<div class="container" style="margin-top:50px;">
+   <div class="col-md-12">
+      <div class="row">
+         <div class="col-md-6">
+            <button class="btn btn-info" data-toggle="modal" data-target="#term_ppo"><i class="fa fa-arrow-circle-right" aria-hidden="true"></i> Rules/Regulations for Termination of PPO</button>
+         </div>
+         <div class="col-md-6">
+            <button class="btn btn-info" data-toggle="modal" data-target="#leaves"><i class="fa fa-arrow-circle-right" aria-hidden="true"></i> Rules/Regulations for taking Leaves from Industry</button>
+         </div>
+      </div>
+      <br><br>        	  
+      <br>
+      <center>
+         <h5>Upcoming Drives (Tentatively)</h5>
+      </center></br>
+      
+         <div class="row">
+            <center><div class='alert alert-warning'><strong><i class='fa fa-bullhorn'></i> No Upcoming Drives are there.We will inform you. Stay Tuned with us</strong></center></div>         <br>
+         <center>
+            <h5>Upcoming Workshops (Tentatively)</h5>
+         </center><br>
+                     <div class="row">
+               <center><div class='alert alert-warning'><strong><i class='fa fa-bullhorn'></i> No Upcoming Workshops are there.We will inform you. Stay Tuned with us</strong></div></center></div>
+         </div>
+   </div>
+   <br><br>
+</body>
+<?php include( 'includes/footer.html' );
+?>
