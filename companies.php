0b557248de7e4ef15554144fd894e6ee4c587bcb
Madhu <67431480+Madh0906@users.noreply.github.com>
2020-07-01 12:43:47 +0530

    companies page added..

---
+++ companies.php
@@ -0,0 +1,253 @@
+<?php include( 'includes/header.html' );
+?>
+<?php include( 'includes/nav_bar.html' );
+?>
+<body>
+	<div class="container">
+	<div class="col-md-12" style="margin-top:50px;">
+			<div class="alert alert-success">
+				<strong><i class="fa fa-smile-o"></i> We are happy with you. You are doing Internship at Vedantu. </strong>
+			</div>				
+			<div class="card">
+				<div class="card-body">
+					<h4 class="card-title">Read carefull before applying for Placement/Internship</h4>
+					<i class="fa fa-hand-o-right"></i> Follow the instructions carefully given in JNF or INF before registering to any company.
+					<br>
+					<span class="fa fa-hand-o-right" style="font-size:18px"></span> Once you registered as per the info from JNF or INF he/she need to attend the complete Drive process of the company.
+					<br>
+					<span class="fa fa-hand-o-right" style="font-size:18px"></span> Once the student got offer from any company you need to strictly follow the terms and conditions of the company without fail by co-ordinating with respective departments
+					<br>
+					<span class="fa fa-hand-o-right" style="font-size:18px"></span> Once if any student registered and not attended for drive process without any valid reason have to accept the rules and regulations of the commitee.
+				</div>
+			</div>
+			<br>
+			<div class="row">
+					<div class="col-md-6">
+						<div class="card">
+							<div class="row no-gutters">
+								<div class="col-md-4">
+									<img src="resources/rguktlogo.png" class="card-img p-3" alt="Alacriti_21_RD">
+								</div>
+								<div class="col-md-8">
+									<div class="card-body">
+										<h5 class="card-title">Alacriti_21_RD</h5>
+										<p>Internship<br>Location : Hyderabad<br>Stipend : 5.5LPA</p>
+										<p>
+											<div class="alert alert-warning">
+												<strong><i class="fa fa-clock-o"></i> Closed on 2020-03-10</strong>
+											</div>												
+										</p>
+									</div>
+								</div>		
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="card">
+							<div class="row no-gutters">
+								<div class="col-md-4">
+									<img src="resources/rguktlogo.png" class="card-img p-3" alt="Alacriti_21_RD">
+								</div>
+								<div class="col-md-8">
+									<div class="card-body">
+										<h5 class="card-title">Alacriti_21_RD</h5>
+										<p>Internship<br>Location : Hyderabad<br>Stipend : 5.5LPA</p>
+										<p>
+											<div class="alert alert-warning">
+												<strong><i class="fa fa-clock-o"></i> Closed on 2020-03-10</strong>
+											</div>												
+										</p>
+									</div>
+								</div>		
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="card">
+							<div class="row no-gutters">
+								<div class="col-md-4">
+									<img src="resources/rguktlogo.png" class="card-img p-3" alt="Alacriti_21_RD">
+								</div>
+								<div class="col-md-8">
+									<div class="card-body">
+										<h5 class="card-title">Alacriti_21_RD</h5>
+										<p>Internship<br>Location : Hyderabad<br>Stipend : 5.5LPA</p>
+										<p>
+											<div class="alert alert-warning">
+												<strong><i class="fa fa-clock-o"></i> Closed on 2020-03-10</strong>
+											</div>												
+										</p>
+									</div>
+								</div>		
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="card">
+							<div class="row no-gutters">
+								<div class="col-md-4">
+									<img src="resources/rguktlogo.png" class="card-img p-3" alt="Alacriti_21_RD">
+								</div>
+								<div class="col-md-8">
+									<div class="card-body">
+										<h5 class="card-title">Alacriti_21_RD</h5>
+										<p>Internship<br>Location : Hyderabad<br>Stipend : 5.5LPA</p>
+										<p>
+											<div class="alert alert-warning">
+												<strong><i class="fa fa-clock-o"></i> Closed on 2020-03-10</strong>
+											</div>												
+										</p>
+									</div>
+								</div>		
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="card">
+							<div class="row no-gutters">
+								<div class="col-md-4">
+									<img src="resources/rguktlogo.png" class="card-img p-3" alt="Alacriti_21_RD">
+								</div>
+								<div class="col-md-8">
+									<div class="card-body">
+										<h5 class="card-title">Alacriti_21_RD</h5>
+										<p>Internship<br>Location : Hyderabad<br>Stipend : 5.5LPA</p>
+										<p>
+											<div class="alert alert-warning">
+												<strong><i class="fa fa-clock-o"></i> Closed on 2020-03-10</strong>
+											</div>												
+										</p>
+									</div>
+								</div>		
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="card">
+							<div class="row no-gutters">
+								<div class="col-md-4">
+									<img src="resources/rguktlogo.png" class="card-img p-3" alt="Alacriti_21_RD">
+								</div>
+								<div class="col-md-8">
+									<div class="card-body">
+										<h5 class="card-title">Alacriti_21_RD</h5>
+										<p>Internship<br>Location : Hyderabad<br>Stipend : 5.5LPA</p>
+										<p>
+											<div class="alert alert-warning">
+												<strong><i class="fa fa-clock-o"></i> Closed on 2020-03-10</strong>
+											</div>												
+										</p>
+									</div>
+								</div>		
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="card">
+							<div class="row no-gutters">
+								<div class="col-md-4">
+									<img src="resources/rguktlogo.png" class="card-img p-3" alt="Alacriti_21_RD">
+								</div>
+								<div class="col-md-8">
+									<div class="card-body">
+										<h5 class="card-title">Alacriti_21_RD</h5>
+										<p>Internship<br>Location : Hyderabad<br>Stipend : 5.5LPA</p>
+										<p>
+											<div class="alert alert-warning">
+												<strong><i class="fa fa-clock-o"></i> Closed on 2020-03-10</strong>
+											</div>												
+										</p>
+									</div>
+								</div>		
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="card">
+							<div class="row no-gutters">
+								<div class="col-md-4">
+									<img src="resources/rguktlogo.png" class="card-img p-3" alt="Alacriti_21_RD">
+								</div>
+								<div class="col-md-8">
+									<div class="card-body">
+										<h5 class="card-title">Alacriti_21_RD</h5>
+										<p>Internship<br>Location : Hyderabad<br>Stipend : 5.5LPA</p>
+										<p>
+											<div class="alert alert-warning">
+												<strong><i class="fa fa-clock-o"></i> Closed on 2020-03-10</strong>
+											</div>												
+										</p>
+									</div>
+								</div>		
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-6">
+						<div class="card">
+							<div class="row no-gutters">
+								<div class="col-md-4">
+									<img src="resources/rguktlogo.png" class="card-img p-3" alt="Alacriti_21_RD">
+								</div>
+								<div class="col-md-8">
+									<div class="card-body">
+										<h5 class="card-title">Alacriti_21_RD</h5>
+										<p>Internship<br>Location : Hyderabad<br>Stipend : 5.5LPA</p>
+										<p>
+											<div class="alert alert-warning">
+												<strong><i class="fa fa-clock-o"></i> Closed on 2020-03-10</strong>
+											</div>												
+										</p>
+									</div>
+								</div>		
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="card">
+							<div class="row no-gutters">
+								<div class="col-md-4">
+									<img src="resources/CDPC.jpg" class="card-img p-3" alt="Alacriti_21_RD">
+								</div>
+								<div class="col-md-8">
+									<div class="card-body">
+										<h5 class="card-title">Alacriti_21_RD</h5>
+										<p>Internship<br>Location : Hyderabad<br>Stipend : 5.5LPA</p>
+										<p>
+											<div class="alert alert-warning">
+												<strong><i class="fa fa-clock-o"></i> Closed on 2020-03-10</strong>
+											</div>												
+										</p>
+									</div>
+								</div>		
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="card">
+							<div class="row no-gutters">
+								<div class="col-md-4">
+									<img src="resources/rgukt.png" class="card-img p-3" alt="Alacriti_21_RD">
+								</div>
+								<div class="col-md-8">
+									<div class="card-body">
+										<h5 class="card-title">Alacriti_21_RD</h5>
+										<p>Internship<br>Location : Hyderabad<br>Stipend : 5.5LPA</p>
+										<p>
+											<div class="alert alert-warning">
+												<strong><i class="fa fa-clock-o"></i> Closed on 2020-03-10</strong>
+											</div>												
+										</p>
+									</div>
+								</div>		
+							</div>
+						</div>
+					</div>
+
+			</div>
+	</div>
+	</div>
+<br><br><br>
+
+<?php include( 'includes/footer.html' );
+?>
